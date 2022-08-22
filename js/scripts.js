8af2af39fcaee88510d1b9bb13571ec371784bd5
tgold83 <ted.jonathan.gold@gmail.com>
2022-08-22 11:40:00 -0700

    add HTML and CSS files, add Html and UI logic to allow user input
    Co-authored-by: William Mentzer <william.mentzer2k@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,23 @@ function Destination(location, landmark, date, notes, mood, weather) {
   this.mood = mood;
   this.weather = weather;
 }
+
+// User Interface Logic
+let passport = new Passport();
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const inputLocation = document.querySelector("input#new-location").value;
+  const inputLandmark = document.querySelector("input#new-landmark").value;
+  const inputDate = document.querySelector("input#new-date").value;
+  const inputNotes = document.querySelector("input#new-note").value;
+  const inputMood = document.querySelector("input#new-mood").value;
+  const inputWeather = document.querySelector("input#new-weather").value;
+  let newPlace = new Destination(inputLocation, inputLandmark, inputDate, inputNotes, inputMood, inputWeather);
+  passport.assignDestination(newPlace);
+  console.log(passport.destinations);
+}
+
+window.addEventListener("load", function (){
+  document.querySelector("form#location-nabber").addEventListener("submit", handleFormSubmission);
+});
